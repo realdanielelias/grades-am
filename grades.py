174643c59cd29059aefc338bed157f516f0b5cb5
Daniel Elias <gitdanielelias@gmail.com>
2024-10-03 21:40:06 -0300

    Adding code

---
+++ grades.py
@@ -0,0 +1,14 @@
+#Get both test grades
+grade1 = float(input("Type the first grade: "))
+grade2 = float(input("Type the second grade: "))
+
+#Calculate the arithmetic mean
+mean = (grade1+grade2)/2
+
+#If the student mean is equal or higher than 7, the student was approved
+if mean >=7:
+    print("Approved")
+
+#If not, the student was reproved
+else:
+    print("Reproved")
